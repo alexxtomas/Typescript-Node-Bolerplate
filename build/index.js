950ef0139eb401ba399535a4035d2e0e7d9f752d
Alex Tomas <atomasllimos@gmail.com>
2022-11-25 12:57:18 +0100

    Remove build folder

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-console.log('hello world');
-const a = 'a';
-module.exports = a;
-//# sourceMappingURL=index.js.map
--- build/tests/test.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-describe('All working', () => {
-    test('Good', () => {
-        const a = 1;
-        expect(a).toBe(1);
-    });
-});
-//# sourceMappingURL=test.test.js.map
